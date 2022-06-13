b559283ea716298e1d13f997c8732ce53f7a5b2c
AryaS77 <94583570+AryaS77@users.noreply.github.com>
2022-06-13 14:35:01 +0700

    Add files via upload

---
+++ FinalProjectPROGTECH/app/src/main/java/com/example/finalprojectprogtech/menu2.kt
@@ -0,0 +1,11 @@
+package com.example.finalprojectprogtech
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class menu2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
